e0ccb2e92e0d98e136818245cd76b00d8f7ddcf3
yanivabir <yanivabir@gmail.com>
2024-07-04 11:32:35 +0100

    allow save multiple times

--- utils.js
+++ utils.js
@@ -1,16 +1,22 @@
 // Save data to REDCap
 function saveDataREDCap() {
+
+    const auto_number = window.record_id == undefined
+
+    console.log(auto_number)
+
     var jspsych_data = jsPsych.data.get().json();
 
     var redcap_record = JSON.stringify([{
-        record_id: 1, // Mandatory, but ignored by REDcap
+        record_id: auto_number ? 1 : window.record_id, // Mandatory, but if auto_number then ignored by REDcap
         prolific_pid: window.prolificPID,
         study_id: window.studyId,
         session_id: window.sessionId,
         start_time: window.startTime,
         exp_code_version: code_version,
         group: window.condition,
-        jspsych_data: jspsych_data
+        jspsych_data: jspsych_data,
+        auto_number: auto_number ? 'true' : 'false'
     }])
 
     fetch('https://h6pgstm0f9.execute-api.eu-north-1.amazonaws.com/prod/submit', {
@@ -30,6 +36,9 @@ function saveDataREDCap() {
     })
     .then(data => {
         console.log(data)
+        if (auto_number){
+            window.record_id = JSON.parse('[' + data[0] + ']')[0]
+        }
     }
     )
     .catch(error => {
