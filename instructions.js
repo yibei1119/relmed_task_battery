e4aca9b129378bd87bfe8e4328ad3377d9f1a6e4
yanivabir <yanivabir@gmail.com>
2024-07-04 14:36:19 +0100

    format instructions

--- instructions.js
+++ instructions.js
@@ -3,17 +3,19 @@ function prepare_instructions() {
     let inst =  [
         {
         type: jsPsychInstructions,
+        css_classes: ['instructions'],
         pages: [
             "<p><b>Welcome to The Card Collector's Challenge!</b></p>\
-                <p>On each turn, the Card Collector will present you with two cards. \
-                    The Card Collector has hidden a coin beneath each card. \
-                    You get to choose which card to flip and reveal the coin underneath.</p>"
+                <p>On each turn, the Card Collector will present you with two cards.</p>\
+                    <p>The Card Collector has hidden a coin beneath each card.</p>\
+                    <p>You get to choose which card to flip and reveal the coin underneath.</p>"
         ],
         show_clickable_nav: true,
         data: {trialphase: "instruction"}
     },
     {
         type: jsPsychHtmlKeyboardResponse,
+        css_classes: ['instructions'],
         stimulus: "<p>Let's try it out! Two cards will appear on the next screen. \
                 Use the 'F' key to choose the left card, or the 'J' key to choose the right card.</p>\
                 <p>Place your fingers on the keyboard as shown below, and press either the 'F' or 'J' key to continue.</p>",
@@ -41,6 +43,7 @@ function prepare_instructions() {
     
     inst = inst.concat([{
         type: jsPsychInstructions,
+        css_classes: ['instructions'],
         pages: [
             "<p>Well done! You found a one-pound coin.</p>",
             "<p>The Card Collector uses 1 penny coins, 50 pence coins, and 1-pound coins. \
@@ -62,6 +65,7 @@ function prepare_instructions() {
     },
     {
         type: jsPsychHtmlKeyboardResponse,
+        css_classes: ['instructions'],
         stimulus: "<p>Let's practice both versions. We'll start with winning coins. \
             You begin with zero coins. Try to collect as many as you can.</p>\
             <p>Place your fingers on the keyboard as shown below, and press either the 'F' or 'J' key to continue.</p>",
@@ -70,6 +74,7 @@ function prepare_instructions() {
     },
     {
         type: jsPsychHtmlKeyboardResponse,
+        css_classes: ['instructions'],
         stimulus: "<p>Well done! You would have earned £X in this game.</p>\
         <p>Now, let's practice avoiding losses. You'll start with £Y of coins. Try to lose as few as possible.</p>\
         <p>Place your fingers on the keyboard as shown below, and press either the 'F' or 'J' key to continue.</p>",
@@ -78,6 +83,7 @@ function prepare_instructions() {
     },
     {
         type: jsPsychInstructions,
+        css_classes: ['instructions'],
         pages: function() {
             let pages = [
                 "<p>Well done! You would have earned £X in this game.</p>"
