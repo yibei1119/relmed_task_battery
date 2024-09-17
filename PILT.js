d47fc8389ecfb8aa4ef69b3d9d36d9a3d6031401
yanivabir <yanivabir@gmail.com>
2024-09-17 15:43:19 +0100

    post pilt test

--- PILT.js
+++ PILT.js
@@ -17,6 +17,216 @@ const inter_block_msg = {
     on_finish: () => {window.skipThisBlock = false}
 }
 
+// Post-PILT test trial
+const test_trial = {
+    timeline: [
+        kick_out,
+        fullscreen_prompt,
+    {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: () => {
+            return `<style>
+                .optionBox {
+                    margin: auto;
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: space-between;
+                }
+                
+                .optionSide {
+                    display: grid;
+                    flex-direction: column;
+                    height: 70vh;
+                    width: 25vw;
+                    max-width: 1000px;
+                    position: relative;
+                }
+                
+                .optionSide img {
+                    height: auto;
+                    width: 20vw;
+                    max-width: 500px;
+                   /* flex-direction: column;*/
+                    margin: auto;
+                    grid-column: 1;
+                    grid-row: 1;
+                }
+
+                #rightImg, #leftImg {
+                    border: 1px solid darkgrey;
+                }
+                
+                .helperTxt {
+                    display: flex;
+                    justify-content: space-around;
+                    margin: auto;
+                    width: 12vw;
+                    max-width: 325px;
+                    
+                    text-wrap: normal;
+                    font-size: 1.5rem;
+                    font-weight: bold;
+                    line-height: 3rem;
+                }
+                
+                .coin {
+                    visibility: hidden;
+                    max-width: 12vw;
+                    margin: auto;
+                }
+                
+            </style>
+            <div id="optionBox" class="optionBox">
+                <div id='left' class="optionSide">
+                    <img id='leftImg' src=imgs/${jsPsych.evaluateTimelineVariable('stimulus_left')}></img> 
+                </div>
+                <div class="helperTxt">
+                    <h2 id="centerTxt">?</h2>
+                </div>
+                <div id='right' class="optionSide">
+                    <img id='rightImg' src=imgs/${jsPsych.evaluateTimelineVariable('stimulus_right')}></img>
+                </div>
+            </div>`
+        },
+        choices: ["ArrowRight", "ArrowLeft"],
+        trial_duration: defaultMaxRespTime,
+        data: {
+            trialphase: "PILT_test"
+        }
+    },
+    {
+        timeline: [
+            {
+                type: jsPsychHtmlKeyboardResponse,
+                data: {
+                    trialphase: "PILT_test_timeout"
+                },
+                stimulus: () => {
+                    return `<style>
+                        .optionBox {
+                            margin: auto;
+                            display: flex;
+                            flex-direction: row;
+                            justify-content: space-between;
+                        }
+                        
+                        .optionSide {
+                            display: grid;
+                            flex-direction: column;
+                            height: 70vh;
+                            width: 25vw;
+                            max-width: 1000px;
+                            position: relative;
+                        }
+                        
+                        .optionSide img {
+                            height: auto;
+                            width: 20vw;
+                            max-width: 500px;
+                           /* flex-direction: column;*/
+                            margin: auto;
+                            grid-column: 1;
+                            grid-row: 1;
+                        }
+        
+                        #rightImg, #leftImg {
+                            border: 1px solid darkgrey;
+                        }
+                        
+                        .helperTxt {
+                            display: flex;
+                            justify-content: space-around;
+                            margin: auto;
+                            width: 12vw;
+                            max-width: 325px;
+                            
+                            text-wrap: normal;
+                            font-size: 1.5rem;
+                            font-weight: bold;
+                            line-height: 3rem;
+                        }
+                        
+                        .coin {
+                            visibility: hidden;
+                            max-width: 12vw;
+                            margin: auto;
+                        }
+                        
+                    </style>
+                    <div id="optionBox" class="optionBox">
+                        <div id='left' class="optionSide">
+                            <img id='leftImg' src=imgs/${jsPsych.evaluateTimelineVariable('stimulus_left')}></img> 
+                        </div>
+                        <div class="helperTxt">
+                            <h2 id="centerTxt">Please respond more quickly!</h2>
+                        </div>
+                        <div id='right' class="optionSide">
+                            <img id='rightImg' src=imgs/${jsPsych.evaluateTimelineVariable('stimulus_right')}></img>
+                        </div>
+                    </div>`
+                },
+                choices: "NO_KEYS",
+                trial_duration: 1500
+            }
+        ],
+        conditional_function: () => {
+            let missed = jsPsych.data.get().filter({
+                trialphase: "PILT_test"
+            }).last(1).select("response").values[0] == null
+
+            if (missed){
+                // Update warning count
+                var up_to_now = parseInt(jsPsych.data.get().last(1).select('n_warnings').values);
+                jsPsych.data.addProperties({
+                n_warnings: up_to_now + 1
+                });
+            }
+
+            return missed
+        }
+    },
+    {
+        type: jsPsychHtmlKeyboardResponse,
+        choices: "NO_KEYS",
+        stimulus: "",
+        data: {
+            trialphase: "PILT_test_ITI"
+        },
+        trial_duration: 0,
+        post_trial_gap: 600
+    }
+]
+};
+
+// Build post_PILT test block
+function build_post_PILT_test(structure){
+    
+    // Preload images
+    let test = [
+        {
+            type: jsPsychPreload,
+            images: structure[1]
+                .flatMap(item => [item.stimulus_right, item.stimulus_left])
+                .map(value => `imgs/${value}`),
+            post_trial_gap: 800
+        }
+    ];
+
+    // Push blocks from structure
+    for (let i=0; i < structure.length; i++){
+
+        // Push block                
+        test.push({
+            timeline: [
+                test_trial
+            ],
+            timeline_variables: structure[i]
+        })
+    }
+
+    return test
+}
+
 // PILT trial
 const PLT_trial =  {
     timeline: [
@@ -183,6 +393,7 @@ function build_PLT_task(structure, insert_msg = true){
 // Load PILT sequences from json file
 async function load_squences(session) {
     try {
+        // Fetch PILT sequences
         const response = await fetch('pilot2.json');
         
         if (!response.ok) {
@@ -192,6 +403,17 @@ async function load_squences(session) {
         const sess_structure = structure[session - 1];
 
         window.totalBlockNumber = sess_structure.length
+
+        // Fetch post-PILT test sequences
+        const test_response = await fetch('pilot2_test.json');
+
+        if (!test_response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const test_structure = await test_response.json();
+        const test_sess_structure = test_structure[session - 1];
+
 
         // Fetch the current time from the World Time API
         const time_resp = await fetch('https://worldtimeapi.org/api/timezone/Europe/London');
@@ -206,7 +428,7 @@ async function load_squences(session) {
 
         window.startTime = format_date_from_string(verifiedDateString)
 
-        run_full_experiment(sess_structure);
+        run_full_experiment(sess_structure, test_sess_structure);
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
     }
