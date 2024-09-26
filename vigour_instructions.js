125b1c051ae5ddb2a182821e517720d6eeff4178
Haoyang Lu <haoyang.lu@ucl.ac.uk>
2024-09-26 22:38:35 +0100

    feat: Change introduction interface
    - Add unlimited attempts for shaking
    - Update rule introduction part before first trial starts

--- vigour_instructions.js
+++ vigour_instructions.js
@@ -1,4 +1,4 @@
-// instruction-page.js
+// vigour_instructions.js
 
 // Instruction page configuration
 const instructionPage = {
@@ -7,39 +7,85 @@ const instructionPage = {
   choices: "NO_KEYS",
   trial_duration: null,
   on_load: function () {
+    updatePersistentCoinContainer();
+    observeResizing('coin-container', updatePersistentCoinContainer);
+    
     let shakeCount = 0;
-    const instructionText = document.getElementById('instruction-text');
-    const buttonContainer = document.getElementById('button-container');
-    const restartButton = document.getElementById('restart-button');
-    const continueButton = document.getElementById('continue-button');
-
-    // Keyboard listener
+    updateInstructionText(shakeCount);
+    const bottomContainer = document.getElementById('bottom-container');
     let keyboardListener = setupKeyboardListener(handleSpacebar);
 
-    // Handle spacebar key press
     function handleSpacebar() {
       shakeCount++;
       shakePiggy();
       updateInstructionText(shakeCount);
 
-      if (shakeCount === 3) {
-        dropCoin(0);  // Out-of-circulation coin for instruction
-        buttonContainer.style.visibility = 'visible';
-        jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
+      if (shakeCount % 5 === 0) {
+        dropCoin(0);
+        bottomContainer.style.visibility = 'visible';
       }
     }
 
-    // Restart the trial
     function restart() {
       shakeCount = 0;
       updateInstructionText(shakeCount);
-      buttonContainer.style.visibility = 'hidden';
+      bottomContainer.style.visibility = 'hidden';
+      jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
       keyboardListener = setupKeyboardListener(handleSpacebar);
+      const coinContainer = document.getElementById('coin-container');
+      coinContainer.innerHTML = '';
     }
 
-    // Button event listeners
-    restartButton.addEventListener('click', restart);
-    continueButton.addEventListener('click', jsPsych.finishTrial);
+    document.getElementById('restart-button').addEventListener('click', restart);
+    document.getElementById('continue-button').addEventListener('click', jsPsych.finishTrial);
+  },
+  on_finish: function () {
+    jsPsych.pluginAPI.cancelAllKeyboardResponses();
+  }
+};
+
+// Before starting the first trial
+const startFirstTrial = {
+  type: jsPsychHtmlKeyboardResponse,
+  choices: [' '],
+  stimulus: `
+  <div id="instruction-text" style="text-align: left">
+    <p><strong>You will now play a few minutes of this game, collecting coins!</strong></p>
+    
+    <p>Throughout the game, you will see different piggy banks with unique appearances:</p>
+    <ul>
+        <li><img src="imgs/saturate-icon.png" style="height:1.3em; transform: translateY(0.2em)"> <span class="highlight">Vividness</span> of piggy colors: Indicates how hard you need to shake it.</li>
+        <li><img src="imgs/tail-icon.png" style="height:1.3em; transform: translateY(0.2em)"> <span class="highlight">Tail length</span>: Longer piggy tails = more valuable coins.</li>
+    </ul>
+    
+    <p>Types of coins you can win:</p>
+    <div class="instruct-coin-container">
+        <div class="instruct-coin">
+            <img src="imgs/1p.png" alt="1 Penny">
+            <p>1 Penny</p>
+        </div>
+        <div class="instruct-coin">
+            <img src="imgs/2p.png" alt="2 Pence">
+            <p>2 Pence</p>
+        </div>
+        <div class="instruct-coin">
+            <img src="imgs/5p.png" alt="5 Pence">
+            <p>5 Pence</p>
+        </div>
+        <div class="instruct-coin">
+            <img src="imgs/10p.png" alt="10 Pence">
+            <p>10 Pence</p>
+        </div>
+    </div>
+    
+    <p><span class="highlight">Your bonus</span>: You will keep all the coins from a randomly selected piggy bank at the end of the game.</p>
+    
+    <p><strong>When you're ready, press the <span class="spacebar-icon">Spacebar</span> to start!</strong></p>
+    </div>
+      `,
+  on_finish: function (data) {
+    const seed = jsPsych.randomization.setSeed();
+    data.rng_seed = seed;
   }
 };
 
@@ -49,19 +95,25 @@ function generateInstructStimulus() {
     <div class="experiment-wrapper">
       <!-- Upper Information (Instructions) -->
       <div id="instruction-container">
-        <div id="instruction-text">Press <span class="spacebar-icon">Spacebar</span> to shake the piggy bank...</div>
+        <div id="instruction-text"></div>
       </div>
 
       <!-- Middle Row (Piggy Bank & Coins) -->
       <div id="experiment-container">
         <div id="coin-container"></div>
-        <img id="piggy-bank" src="imgs/piggy-bank.png" alt="Piggy Bank">
+        <div id="piggy-container">
+          <!-- Piggy Bank Image -->
+          <img id="piggy-bank" src="imgs/piggy-bank.png" alt="Piggy Bank">
+        </div>
       </div>
 
       <!-- Lower Information (Buttons) -->
-      <div id="button-container">
-        <button id="restart-button" class="jspsych-btn">Restart</button>
-        <button id="continue-button" class="jspsych-btn">Continue</button>
+      <div id="bottom-container" style="visibility: hidden">
+        <p>Press <span style="font-weight: bold;">Restart</span> to retry from the beginning, or <span style="font-weight: bold;">Continue</span> to proceed.</p>
+        <div id="button-container">
+          <button id="restart-button" class="jspsych-btn">Restart</button>
+          <button id="continue-button" class="jspsych-btn">Continue</button>
+        </div>
       </div>
     </div>
   `;
@@ -70,12 +122,19 @@ function generateInstructStimulus() {
 // Function to update instruction text based on shake count
 function updateInstructionText(shakeCount) {
   const messages = [
-    'Press <span class="spacebar-icon">Spacebar</span> to shake the piggy bank...',
-    'Press <span class="spacebar-icon">Spacebar</span> to shake the piggy bank... shake...',
-    'Press <span class="spacebar-icon">Spacebar</span> to shake the piggy bank... shake... and shake again!',
-    'Well done! You just got a coin out of the piggy bank!<br /> Press <span style="font-weight: bold;">Continue</span> to proceed, or <span style="font-weight: bold;">Restart</span> to try again.'
+    'Press <span class="spacebar-icon">Spacebar</span> to shake this piggy bank!',
+    '<p>Press <span class="spacebar-icon">Spacebar</span> to shake this piggy bank!</p><p>You can keep pressing <span class="spacebar-icon">Spacebar</span> to keep on shaking...</p>',
+    '<p>Well done! You just got a coin out of the piggy bank!</p><p><span class="highlight">You can always keep pressing for more coins.</span> Try getting some more!</p>'
   ];
-  document.getElementById('instruction-text').innerHTML = messages[shakeCount];
+  let messageIndex = 0;
+  if (shakeCount < 1) {
+    messageIndex = 0;
+  } else if (shakeCount >= 1 && shakeCount < 5) {
+    messageIndex = 1;
+  } else {
+    messageIndex = 2;
+  }
+  document.getElementById('instruction-text').innerHTML = messages[messageIndex];
 }
 
 // Function to set up keyboard listener
