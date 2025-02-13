e5e3534cd8e14b7753266ab3a4eaf5f3f8abba70
yanivabir <yanivabir@gmail.com>
2025-02-13 13:55:40 +0000

    improve wm instructions

--- PILT_instructions.js
+++ PILT_instructions.js
@@ -364,7 +364,8 @@ const WM_instructions = [
             '<p>Next, you will return to the card choosing game.</p>\
                 <p>Your goal remains to add as much money as you can to your safe.</p>',
             `<p>This time, you will choose between three cards on every turn.<p>
-            <p>In every triplet, one picture card will have only one pound and fifty pence coins behind it, while the other two cards will have only pennies.<p>`,
+            <p>In every triplet, one picture card will have only one pound and fifty pence coins behind it, while the other two cards will have only pennies.<p>
+            <p>You can earn more by learning which is the better picture card in each triplet and choosing that card when you next see same triplet.</p>`,
             `<p>Use the right arrow key to choose the card on the right, the left arrow key to choose the card on the left, 
             and <b>use the upwards arrow key to choose the card in the middle.</b>
             `
