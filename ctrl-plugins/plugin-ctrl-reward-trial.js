dc8c127e2c38f50465c4c3673f5f95d98e2fb364
Haoyang Lu <haoyang.lu@ucl.ac.uk>
2025-03-11 01:47:48 +0000

    Fix jsPsychRewardShip simulation speed up

--- ctrl-plugins/plugin-ctrl-reward-trial.js
+++ ctrl-plugins/plugin-ctrl-reward-trial.js
@@ -326,16 +326,15 @@ var jsPsychRewardShip = (function (jspsych) {
       const choiceToKey = {"left": "ArrowLeft", "right": "ArrowRight"};
       const data = this.create_simulation_data(trial, simulation_options);
       const display_element = this.jsPsych.getDisplayElement();
-
-      this.trial(display_element, trial);
-
+      
       // Faster visual simulation
       if (simulation_options.speed_up) {
         trial.reward_decision = trial.reward_decision / simulation_options.speed_up_factor;
         trial.reward_effort = trial.reward_effort / simulation_options.speed_up_factor; 
         trial.post_trial_gap = trial.post_trial_gap / simulation_options.speed_up_factor;
       }
-
+      
+      this.trial(display_element, trial);
       load_callback();
 
       if (data.rt!== null) {
