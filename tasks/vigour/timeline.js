547aed7956e58448d1a6c8f0dc6b0b47fb89845f
yanivabir <yanivabir@gmail.com>
2025-08-24 13:59:12 +0100

    refactor: assemble final timeline in timeline.js

--- tasks/vigour/timeline.js
+++ tasks/vigour/timeline.js
@@ -1,3 +1,13 @@
 import { fullscreen_prompt, kick_out, updateState } from '/core/utils/index.js';
-import { createPersistentCoinContainer, removePersistentCoinContainer, piggyBankTrial } from './utils.js';
+import { createCoreVigourTimeline } from './utils.js';
+import { vigour_instructions } from './instructions.js';
 
+function createVigourTimeline(settings) {
+    const vigourTimeline = [
+        preloadVigour(settings),
+        vigour_instructions,
+        ...createCoreVigourTimeline(settings),
+    ];
+    
+    return vigourTimeline;
+}
