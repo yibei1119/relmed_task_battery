e8a0eb0073d5007d65581818cfc6d422833fdfd2
yanivabir <yanivabir@gmail.com>
2025-08-24 13:59:24 +0100

    refactor: export func

--- tasks/vigour/timeline.js
+++ tasks/vigour/timeline.js
@@ -2,7 +2,7 @@ import { fullscreen_prompt, kick_out, updateState } from '/core/utils/index.js';
 import { createCoreVigourTimeline } from './utils.js';
 import { vigour_instructions } from './instructions.js';
 
-function createVigourTimeline(settings) {
+export function createVigourTimeline(settings) {
     const vigourTimeline = [
         preloadVigour(settings),
         vigour_instructions,
