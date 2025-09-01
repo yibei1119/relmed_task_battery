e79d1cc664564c37b20c0feb4dd1581fb2ee5f23
yanivabir <yanivabir@gmail.com>
2025-09-01 10:14:53 +0100

    refactor: index.js

---
+++ tasks/open-text/index.js
@@ -0,0 +1,7 @@
+/**
+ * Core open-text task index - centralizes all function exports
+ * Provides a single entry point for importing open-text task functions across the experiment.
+ */
+
+// Import and re-export all utility modules
+export * from './timeline.js';
