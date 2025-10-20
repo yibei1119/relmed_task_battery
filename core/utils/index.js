352c81962c82b98e00a9001b94f25f29e879076f
yibei1119 <yibei1119@outlook.com>
2025-10-20 15:25:42 +0200

    Update index.js

--- core/utils/index.js
+++ core/utils/index.js
@@ -4,10 +4,10 @@
  */
 
 // Import and re-export all utility modules
-export * from 'https://yibei1119.github.io/core/utils//bonus.js';
-export * from 'https://yibei1119.github.io/core/utils/calculations.js';
-export * from 'https://yibei1119.github.io/core/utils/data-handling.js';
-export * from 'https://yibei1119.github.io/core/utils/participation-validation.js';
-export * from 'https://yibei1119.github.io/core/utils/setup.js';
-export * from 'https://yibei1119.github.io/core/utils/resumption.js';
+export * from 'https://yibei1119.github.io/relmed_task_battery/core/utils//bonus.js';
+export * from 'https://yibei1119.github.io/relmed_task_battery/core/utils/calculations.js';
+export * from 'https://yibei1119.github.io/relmed_task_battery/core/utils/data-handling.js';
+export * from 'https://yibei1119.github.io/relmed_task_battery/core/utils/participation-validation.js';
+export * from 'https://yibei1119.github.io/relmed_task_battery/core/utils/setup.js';
+export * from 'https://yibei1119.github.io/relmed_task_battery/core/utils/resumption.js';
 
